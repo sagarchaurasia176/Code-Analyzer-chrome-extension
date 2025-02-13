a3342327cefb2a4d0f6a4e30eaa356c38aae8d72
sagarchaurasia176 <101509099+sagarchaurasia176@users.noreply.github.com>
2025-02-13 20:06:55 +0530

    folder updated

--- src/components/content.tsx
+++ src/components/content.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 const Content = () => {
   return (
     <div>
+      {/* here we are creating the button for the analyze button */}
       <button style={{backgroundColor:"black", color:"white" , display:"flex" , justifyContent:"flex-end"}}>Analyze</button>
     </div>
   )
