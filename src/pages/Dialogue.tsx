e723a0ee2841cb3a57c15b7dd14a6db9ba6d501f
sagarchaurasia176 <101509099+sagarchaurasia176@users.noreply.github.com>
2025-02-13 20:14:36 +0530

    folder upadted

--- src/pages/Dialogue.tsx
+++ src/pages/Dialogue.tsx
@@ -4,6 +4,13 @@ const Dialogue = () => {
   return (
     <div>
       {/* chat appeared  */}
+      {/* chat box */}
+      {/* chat box will be appear when the button is clicked */}
+      {/* chat box will be appear when the button is clicked */}
+      {/* chat box will be appear when the button is clicked */}
+      {/* chat box will be appear when the button is clicked */}
+      {/* chat box will be appear when the button is clicked */}
+      {/* chat box will be appear when the button is clicked */}
     </div>
   )
 }
